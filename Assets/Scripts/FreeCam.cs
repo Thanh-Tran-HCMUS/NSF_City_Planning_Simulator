e5d6c163a609a92dcbf59bebb57c0fe91ca2e400
tndthanh <tndthanh@selab.hcmus.edu.vn>
2022-01-09 02:28:34 +0700

    Limit camera view to increase performance
    - Turn off all shadows on District 1 and ThuDuc city scene
    - Change camera control to keyboard and middle scroll wheel mouse
    - Limit the range of the field of view when control with middle mouse on both scene

--- Assets/Scripts/FreeCam.cs
+++ Assets/Scripts/FreeCam.cs
@@ -7,6 +7,8 @@ public class FreeCam : MonoBehaviour
     public float speedFast = 45f;
     public float mouseSensX = 2f;
     public float mouseSensY = 2f;
+    public float minPosY;
+    public float maxPosY;
     float rotY;
     float speed;
 
@@ -21,30 +23,56 @@ public class FreeCam : MonoBehaviour
         {
             //Cursor.visible = true;
         }
-
-        if (Input.GetMouseButton(1))
+        //rotY += Input.GetAxis("Mouse ScrollWheel") * mouseSensY;
+        //rotY = Mathf.Clamp(rotY, -80f, 80f);
+        //float rotX = transform.localEulerAngles.y + (Input.GetAxis("Mouse ScrollWheel") * mouseSensX);
+        //transform.localEulerAngles = new Vector3(0f, rotX, 0f);
+        if (Input.GetMouseButton(2))
         {
-            float rotX = transform.localEulerAngles.y + (Input.GetAxis("Mouse X") * mouseSensX);
-            rotY += Input.GetAxis("Mouse Y") * mouseSensY;
+            //float rotX = transform.localEulerAngles.y + (Input.GetAxis("Mouse X") * mouseSensX);
+            /*rotY += Input.GetAxis("Mouse ScrollWheel") * mouseSensY;
             rotY = Mathf.Clamp(rotY, -80f, 80f);
-            transform.localEulerAngles = new Vector3(-rotY, rotX, 0f);
+            transform.localEulerAngles = new Vector3(-rotY, 0f, 0f);*/
             //Cursor.visible = false;
         }
         float forward = Input.GetAxis("Vertical");
         float side = Input.GetAxis("Horizontal");
+        float zoom = Input.GetAxis("Mouse ScrollWheel");
         if (forward != 0f)
         {
             if (Input.GetKey(KeyCode.LeftShift)) speed = speedFast;
             else speed = speedNormal;
             Vector3 vect = new Vector3(0f, 0f, forward * speed * Time.deltaTime);
-            transform.localPosition += transform.localRotation * vect;
+            transform.localPosition += vect;
+            //transform.localPosition += transform.localRotation * vect;
         }
         if (side != 0f)
         {
             if (Input.GetKey(KeyCode.LeftShift)) speed = speedFast;
             else speed = speedNormal;
             Vector3 vect = new Vector3(side * speed * Time.deltaTime, 0f, 0f);
-            transform.localPosition += transform.localRotation * vect;
+            transform.localPosition += vect;
+            //transform.localPosition += transform.localRotation * vect;
+        }
+        if (zoom != 0f)
+        {
+            speed = speedNormal;
+            Vector3 vect = new Vector3(0f, -zoom * speed * Time.deltaTime * 10, 0f);
+            if (transform.position.y <= maxPosY && transform.position.y >= minPosY)
+                transform.localPosition += vect;
+            if (transform.position.y > maxPosY)
+            {
+                Vector3 temp = transform.position;
+                temp.y = maxPosY;
+                transform.position = temp;
+            }
+            if (transform.position.y < minPosY)
+            {
+                Vector3 temp = transform.position;
+                temp.y = minPosY;
+                transform.position = temp;
+            }
+            //transform.localPosition += transform.localRotation * vect;
         }
     }
 }
