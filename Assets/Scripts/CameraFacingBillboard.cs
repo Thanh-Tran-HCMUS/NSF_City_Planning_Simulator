0d7cd2b72a6d8f484e790d14ed8f1de4f3d89afc
Thanh-Tran-HCMUS <tndthanh@selab.hcmus.edu.vn>
2022-05-10 01:20:04 +0700

    Design route path and make camera follow
    Add VR functions:
    - Design route path for 8 scenes
    - Make camera follow defined route

--- Assets/Scripts/CameraFacingBillboard.cs
+++ Assets/Scripts/CameraFacingBillboard.cs
@@ -9,6 +9,7 @@ public class CameraFacingBillboard : MonoBehaviour {
     //Orient the camera after all movement is completed this frame to avoid jittering
     void LateUpdate()
     {
+        m_Camera = Camera.main;
         transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.forward,
             Camera.main.transform.rotation * Vector3.up);
     }
